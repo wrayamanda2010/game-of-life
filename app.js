0c35cc63a1042dba5457d2ff5573c439621c7f7e
Amanda Wray <amandawray2010@gmail.com>
2018-03-01 16:06:53 -0700

    everything but intervals

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-//Grid dimensions
+  // GRID DIMENSIONS
   const maxRows = 20
   const maxCols = 20
-  //Grid Data Storage
+
+  // GRID DATA STORAGE
   let activeCells = {}
 
   // SET INITIAL UI STATE
@@ -14,35 +15,107 @@ $(document).ready(function () {
   $('#start-tick-btn').click(startTickBtnClick)
   $('#stop-tick-btn').click(stopTickBtnClick)
 
+  // DRAW INITIAL GRID
   displayUIGrid()
 
-    // UI GRID FUNCTIONS
-    function displayUIGrid() {
-      for (let i = 0; i < maxRows; i++) {
-        const row = $('<tr>')
-        for (let j = 0; j < maxCols; j++) {
-          const cell = $('<td>')
-          const rowColString = `${i},${j}`
-          cell.attr('data-row-col', rowColString)
-          cell.click(uiGridCellClick)
-          row.append(cell)
+  // UI GRID FUNCTIONS
+  function displayUIGrid() {
+    for (let i = 0; i < maxRows; i++) {
+      const row = $('<tr>')
+      for (let j = 0; j < maxCols; j++) {
+        const cell = $('<td>')
+        const rowColString = `${i},${j}`
+        cell.attr('data-row-col', rowColString)
+        cell.click(uiGridCellClick)
+        row.append(cell)
+      }
+      $('#ui-grid').append(row)
+    }
+  }
+
+  function populateUIGrid() {
+    $('td').removeClass('on')
+    Object.keys(activeCells).forEach((p) => {
+      $(`td[data-row-col="${p}"]`).addClass('on')
+    })
+  }
+
+  // SIMULATION FUNCTIONS
+  function oneTick() {
+    // THE NEW GENERATION OF CELLS
+    const nextTickActiveCells = {}
+
+    // LIVE CELLS
+    for (rowColString of Object.keys(activeCells)) {
+      const n = countNeighbors(rowColString)
+      if (n === 2 || n === 3) {
+        nextTickActiveCells[rowColString] = true
+      }
+    }
+
+    // DEAD CELLS
+    for (let i = 0; i < maxRows; i++) {
+      for (let j = 0; j < maxCols; j++) {
+        const rowColString = `${i},${j}`
+        if (!(rowColString in activeCells) && countNeighbors(rowColString) === 3) {
+          nextTickActiveCells[rowColString] = true
         }
-        $('#ui-grid').append(row)
       }
     }
 
-    function uiGridCellClick(event){
-      event.preventDefault()
-      const rowColString = $(this).attr('data-row-col')
-      $(this).toggleClass('on')
+    // POPULATE NEXT
+    activeCells = nextTickActiveCells
+    populateUIGrid()
+  }
 
-      if($(this).hasClass('on')){
-        activeCells[rowColString] = true
-      }else{
-        delete activeCells[rowColString]
+  function countNeighbors(rowColString) {
+    let neighborCount = 0
+    const n = neighborhood(rowColString)
+    for (let k = 0; k < n.length; k++) {
+      if (n[k] in activeCells) {
+        neighborCount++
       }
-
     }
+    return neighborCount
+  }
+
+  function neighborhood(rowColString) {
+    const center = rowColString.split(',').map((x) => +x)
+    const row = center[0]
+    const col = center[1]
+
+    const upRow = row > 0 ? row - 1 : maxRows - 1
+    const downRow = row < maxRows - 1 ? row + 1 : 0
+    const leftCol = col > 0 ? col - 1 : maxCols - 1
+    const rightCol = col < maxCols -1 ? col + 1 : 0
+
+    const north = [upRow, col]
+    const south = [downRow, col]
+    const east = [row, rightCol]
+    const west = [row, leftCol]
+    const northWest = [upRow, leftCol]
+    const southWest = [downRow, leftCol]
+    const southEast = [downRow, rightCol]
+    const northEast = [upRow, rightCol]
+
+    const directions = [north, south, east, west, northWest, southWest, southEast, northEast]
+
+    return directions.map((p) => `${p[0]},${p[1]}`)
+  }
+
+  // CELL EVENT HANDLERS
+  function uiGridCellClick(event) {
+    event.preventDefault()
+    const rowColString = $(this).attr('data-row-col')
+    $(this).toggleClass('on')
+
+    if ($(this).hasClass('on')) {
+      activeCells[rowColString] = true
+    }
+    else {
+      delete activeCells[rowColString]
+    }
+  }
 
   // BUTTON EVENT HANDLERS
   function stopTickBtnClick(event) {
@@ -54,12 +127,11 @@ $(document).ready(function () {
     $('#start-tick-btn').removeAttr('disabled')
 
     $('#stop-tick-btn').attr('disabled', true)
-
-
   }
 
   function oneTickBtnClick(event) {
     event.preventDefault()
+    oneTick()
     console.log('oneTickBtnClick()')
   }
 
@@ -72,14 +144,12 @@ $(document).ready(function () {
     $('#start-tick-btn').attr('disabled', true)
 
     $('#stop-tick-btn').removeAttr('disabled')
-
-
   }
 
   function clearBtnClick(event) {
     event.preventDefault()
     $('td').removeClass('on')
-    activeCells - {};
+    activeCells = {}
   }
 
   function dumpConfigBtnClick(event) {
